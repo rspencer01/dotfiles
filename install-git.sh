7797667111c3ea777eec117d895d8d0434dc01f1
Robert Spencer <robert.spencer94@gmail.com>
2015-05-31 14:34:29 +0200

    Install git

---
+++ install-git.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt-get install git
+
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+git config --global user.email "robert.spencer94@gmail.com"
+git config --global user.name "Robert Spencer"
