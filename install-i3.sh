b046fc218996cc31e9c917d6a9e1d70b8bea5351
Robert Spencer <robert.spencer94@gmail.com>
2015-05-31 14:44:36 +0200

    Removed commented stuff

--- install-i3.sh
+++ install-i3.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-#echo "deb http://debian.sur5r.net/i3/ $(lsb_release -c -s) universe" | sudo tee --append /etc/apt/sources.list
-#sudo apt-get update
-#sudo apt-get --allow-unauthenticated install sur5r-keyring
-#sudo apt-get update
-#sudo apt-get install i3 pavucontrol imagemagic xautolock
+echo "deb http://debian.sur5r.net/i3/ $(lsb_release -c -s) universe" | sudo tee --append /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get --allow-unauthenticated install sur5r-keyring
+sudo apt-get update
+sudo apt-get install i3 pavucontrol imagemagic xautolock
 
 rm ~/.i3 -r
 ln -s dotfiles/.i3 ~/.i3
+
+rm ~/.i3status.conf
+ln -s ~/.i3/.i3status.conf ~/.i3status.conf
