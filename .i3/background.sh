9bf060d752edee9b71e2d184dff165a6d0bd0ca8
Robert Spencer <robert.spencer94@gmail.com>
2016-03-26 20:42:48 +0200

    Allow folders in background images location

--- .i3/background.sh
+++ .i3/background.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-feh --bg-fill /home/robert/Pictures/Desktop/`ls /home/robert/Pictures/Desktop/ | sort -R | tail -n 1`
+feh --bg-fill /home/robert/Pictures/Desktop/`ls /home/robert/Pictures/Desktop/ -p | grep -v / | sort -R | tail -n 1`
